6dd0697f3bf75cf01bbee1eb939606aed8339a4a
Menglin Yu <myu311@gatech.edu>
2020-01-15 00:51:27 -0500

    add an API root comment

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,8 @@
 export const API_ROOT = 'http://35.222.115.99:8080/api/v1';
 //server address on Y's GCP
 //export const API_ROOT = 'http://35.202.233.250:8080';
+//server address on Lai's GCP
+//https://around-dot-around-179500.appspot.com/api/v
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const GEO_OPTIONS =  {
     maximumAge: 3600000,
